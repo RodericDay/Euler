6df3b867e3b35c07f88d5248cce40a506a064f0c
Roderic Day <roderic.day@gmail.com>
2016-02-14 15:50:27 -0500

    py204

---
+++ python3/problem00204_hammingnumbers.py
@@ -0,0 +1,28 @@
+'''
+A Hamming number is a positive number which has no prime factor larger than 5.
+So the first few Hamming numbers are 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15.
+There are 1105 Hamming numbers not exceeding 10^8.
+
+We will call a positive number a generalised Hamming number of type n, if it has
+no prime factor larger than n.
+Hence the Hamming numbers are the generalised Hamming numbers of type 5.
+
+How many generalised Hamming numbers of type 100 are there which don't exceed 10^9?
+'''
+
+import math
+from Euler import prime_sieve
+
+lim = 10**9+1
+primes = list(prime_sieve(100))
+
+def max_pow(lim, x):
+    return math.ceil(math.log(lim)/math.log(x))
+
+def recurse(lim, xs):
+    x = xs.pop()
+    mp = max_pow(lim, x)
+    return sum(recurse(lim/x**p, xs.copy()) for p in range(mp)) if xs else mp
+
+ans = recurse(lim, primes)
+print(ans)
